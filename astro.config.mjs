6e4bac549ed59dbbb7aa758280b5073b0392c992
Thomas Erlang <thomas@erlang.dk>
2025-05-19 16:17:20 +0200

    Autogenerate cmd-vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://synchra.net',
+    site: 'https://docs.synchra.net',
     integrations: [
         icon(),
         starlight({
-            title: 'Synchra',
+            title: 'Synchra Docs',
             logo: {
                 src: './src/assets/tree.svg',
             },
@@ -25,42 +25,7 @@ export default defineConfig({
                 },
                 {
                     label: 'Command vars',
-                    items: [
-                        { label: 'Usage', slug: 'cmd-vars' },
-                        { label: 'General', slug: 'cmd-vars/general' },
-                        { label: 'Category', slug: 'cmd-vars/category' },
-                        { label: 'Accountage', slug: 'cmd-vars/accountage' },
-                        { label: 'Followage', slug: 'cmd-vars/followage' },
-                        { label: 'Countdown', slug: 'cmd-vars/countdown' },
-                        {
-                            label: 'Permit manager',
-                            slug: 'cmd-vars/permit-manager',
-                        },
-                        { label: 'Points', slug: 'cmd-vars/points' },
-                        { label: 'Gambling', slug: 'cmd-vars/gambling' },
-                        { label: 'Quotes', slug: 'cmd-vars/quotes' },
-                        {
-                            label: 'Random number',
-                            slug: 'cmd-vars/random-number',
-                        },
-                        {
-                            label: 'Spotify',
-                            slug: 'cmd-vars/spotify',
-                        },
-                        {
-                            label: 'Time',
-                            slug: 'cmd-vars/time',
-                        },
-                        {
-                            label: 'Title',
-                            slug: 'cmd-vars/title',
-                        },
-                        {
-                            label: 'Weather',
-                            slug: 'cmd-vars/weather',
-                        },
-                        { label: 'Faceit', slug: 'cmd-vars/faceit' },
-                    ],
+                    autogenerate: { directory: 'cmd-vars' },
                 },
             ],
         }),
--- src/content/docs/about/what-is-synchra.md
+++
@@ -1,7 +0,0 @@
----
-title: What is Synchra?
----
-Synchra is a set of tools to help you manage your stream on multiple platforms in one place. With a focus on simplicity and ease of use.
-
-Twitch, YouTube and TikTok support and more to come.
-
